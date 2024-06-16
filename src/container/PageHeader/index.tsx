60b1ce1a13796886004992f79505331569fbd184
Thinh 'Central' Le <thinhmnsd2002@gmail.com>
2024-06-16 10:22:15 +0700

    feat: fix deploy

--- src/container/PageHeader/index.tsx
+++ src/container/PageHeader/index.tsx
@@ -1,36 +1,37 @@
-import { useNavigate } from "react-router-dom"
-import UserMenu from "../UserMenu"
-import { styleOrEmpty } from "@/utils/helpers"
+import { useNavigate } from "react-router-dom";
+import UserMenu from "../UserMenu";
+import { styleOrEmpty } from "@/utils/helpers";
 
 interface IPageHeader {
-    title: string
-    subTitle?: string
+  title: string;
+  subTitle?: string;
 }
 
 const PageHeader = (props: IPageHeader) => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const isSubTitlePresent = !!props.subTitle
+  const isSubTitlePresent = !!props.subTitle;
 
-    return (
-        <div className="mb-3 flex w-full items-center justify-between">
-            <div className="flex items-end gap-2">
-                <h1
-                    onClick={() => isSubTitlePresent && navigate(-1)}
-                    className={`cursor-pointer text-2xl font-medium text-secondary 
+  return (
+    <div className="mb-3 flex w-full items-center justify-between">
+      <div className="flex items-end gap-2">
+        <h1
+          onClick={() => isSubTitlePresent && navigate(-1)}
+          className={`cursor-pointer text-2xl font-medium text-secondary 
                     ${styleOrEmpty(isSubTitlePresent, "cursor-pointer")}`}
-                >
-                    {props.title}
-                </h1>
-                {props.subTitle && (
-                    <>
-                        <span>•</span> <p className="font-medium text-secondary">{props.subTitle}</p>
-                    </>
-                )}
-            </div>
-            <UserMenu />
-        </div>
-    )
-}
+        >
+          {props.title}
+        </h1>
+        {props.subTitle && (
+          <>
+            <span>•</span>{" "}
+            <p className="font-medium text-secondary">{props.subTitle}</p>
+          </>
+        )}
+      </div>
+      <UserMenu />
+    </div>
+  );
+};
 
-export default PageHeader
+export default PageHeader;
